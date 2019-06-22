c6de3740da8daacb0dcfe6e2f30b791d33ff68aa
Chinwa Williams <chinwawilliams@gmail.com>
2019-06-22 23:25:03 +0100

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Learn to design and code React app</h1>
+    <p>Complete course where you learn how to design and code a full react project</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
